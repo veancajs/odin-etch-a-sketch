be0c5b52631c79e51d7dca5004421fa7d7122cf1
veancajs <157962461+veancajs@users.noreply.github.com>
2024-09-28 22:33:31 +0800

    Refactor and enhance Etch-a-Sketch features
    - Update HTML structure by adding a <main> tag for better semantic structure.
    - Add random color generation and opacity scaling for grid hover effect in script.js.
    - Modify grid generation logic to improve clarity and remove redundant elements.
    - Refactor styles in style.css for buttons and grid alignment.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+const main = document.querySelector(".main");
 const container = document.querySelector(".container");
 const gridNumber = document.querySelector("#grid-number");
 
 function getGrid(param) {
     for (let i = 1; i <= param; i++) {
         let div = document.createElement("div");
-        div.innerHTML += `<div class="grid">${i}</div>`;
+        div.innerHTML += `<div class="grid"></div>`;
         container.appendChild(div);
-        for (let j = 1; j <= param; j++) {
-            div.innerHTML += `<div class="grid">${i}</div>`;
+        for (let j = 1; j < param; j++) {
+            div.innerHTML += `<div class="grid"></div>`;
             container.appendChild(div);
         }
     }
@@ -18,14 +19,38 @@ getGrid(16);
 gridNumber.addEventListener("click", function () {
     let value = prompt("Enter the number of squares per side for the new grid:", "");
 
-    let squares = document.querySelectorAll(".grid");
+    const squares = document.querySelectorAll(".grid");
     squares.forEach(square => {
         square.remove();
     })
 
     if (+value >= 1 && +value <= 100) {
         getGrid(+value);
+        setGridColor();
     } else {
         alert("Invalid input. Please enter a number between 1 and 100.")
     }
 });
+
+function getRandomColor() {
+    const color = ["red", "blue", "green", "yellow", "pink", "purple", "brown", "violet", "orange", "gold"];
+    return color[Math.floor(Math.random() * color.length)];
+}
+
+function setGridColor() {
+    const squares = document.querySelectorAll(".grid");
+    let value = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+
+    squares.forEach(function (square) {
+        let random = getRandomColor();
+        let opacityValue = 0;
+        square.addEventListener("mouseenter", function () {
+            square.style.backgroundColor = "red";
+            square.style.backgroundColor = random;
+            square.style.opacity = value[opacityValue];
+            opacityValue++;
+        });
+    })
+}
+
+setGridColor();
